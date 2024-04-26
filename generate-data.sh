3455c8f70627c7161819ca735c4496c87701a719
likaci <3407980+likaci@users.noreply.github.com>
2024-04-26 18:23:57 -0400

    escape double quote; update demo data.js

--- generate-data.sh
+++ generate-data.sh
@@ -6,14 +6,14 @@ DATA_FILE="$DIR/data.js"
 
 
 echo "const repoLogs = [];" > "${DATA_FILE}"
-
 repo forall -j1 -v -c '
+#set -x
 echo $REPO_PATH
 echo "repoLogs.push({ path: \"$REPO_PATH\", logs: [" >> '"${DATA_FILE}"'
-git --no-pager log --since=2024-01-01 --pretty=format:"%h|%cs|%ae|%s" |
+git --no-pager log --since=2024-04-01 --pretty=format:"%h|%cs|%ae|%s" |
 while IFS="|" read -r hash date author message; do
-  message=$(echo "$message" | sed "s/\"/\\\\\"/g" )
-  echo "  { hash: \"$hash\", date: \"$date\", author: \"$author\", message: \"$message\" }," >> '"${DATA_FILE}"'
+  message=$(printf "%s\n" "$message" | sed "s/\"/＂/g")
+  printf "%s\n" "  { hash: \"$hash\", date: \"$date\", author: \"$author\", message: \"$message\" }," >> '"${DATA_FILE}"'
 done
 echo "]});" >> '"${DATA_FILE}"'
 '
